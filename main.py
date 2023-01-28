e83c888e53f36cbb3bd050e23234b992cb3e4442
kosvish <mr.kocta@bk.ru>
2023-01-28 15:07:20 +0300

    created window settings and resolution

---
+++ main.py
@@ -0,0 +1,13 @@
+import pygame
+
+pygame.init()
+
+# create game window
+
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 600
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Sasi")
+
+
