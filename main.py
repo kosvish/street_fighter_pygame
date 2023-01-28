abadcc0641637dfb9c17af3d8e20c03a20dd072c
kosvish <mr.kocta@bk.ru>
2023-01-28 15:20:55 +0300

    Creating a game loop, and functions for displaying the background, loading the image of the background itself

--- main.py
+++ main.py
@@ -3,11 +3,35 @@ import pygame
 pygame.init()
 
 # create game window
-
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 600
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Sasi")
 
+# загрузка заднего фона
+bg_image = pygame.image.load("assets/images/background/background.png").convert_alpha()
 
+
+# функция для прорисовки заднего фона
+def draw_bg():
+    screen.blit(bg_image, (0, 0))
+
+
+# game loop
+run = True
+while run:
+
+    # прорисовка фона
+    draw_bg()
+
+    # event handler
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    # обновление экрана
+    pygame.display.update()
+
+# exit pygame
+pygame.quit()
