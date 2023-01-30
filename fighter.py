f31ccc9c6d173401efaa540a8d4d393b86264e48
kosvish <mr.kocta@bk.ru>
2023-01-30 17:53:28 +0300

    Creating a jump trigger to avoid double jumping and creating a temporary attack function with two types of it

--- fighter.py
+++ fighter.py
@@ -5,6 +5,8 @@ class Fighter():
     def __init__(self, x, y):
         self.rect = pygame.Rect((x, y, 80, 180))
         self.vel_y = 0
+        self.jump = False
+        self.attack_type = 0
 
     def move(self, screen_width, screen_height):
         SPEED = 10
@@ -21,9 +23,19 @@ class Fighter():
         if key[pygame.K_d]:
             dx = SPEED
 
+        # атаки
+        if key[pygame.K_r] or key[pygame.K_t]:
+
+            # определяем какая атака была использована
+            if key[pygame.K_r]:
+                self.attack_type = 1
+            if key[pygame.K_t]:
+                self.attack_type = 2
+
         # прыжок
-        if key[pygame.K_w]:
+        if key[pygame.K_w] and self.jump == False:
             self.vel_y = -30
+            self.jump = True
 
         # добавление гравитации
         self.vel_y += GRAVITY
@@ -36,11 +48,15 @@ class Fighter():
             dx = screen_width - self.rect.right
         if self.rect.bottom + dy > screen_height - 110:
             self.vel_y = 0
+            self.jump = False
             dy = screen_height - 110 - self.rect.bottom
 
         # обновление положения бойца на экране
         self.rect.x += dx
         self.rect.y += dy
+
+    def attack(self):
+        pass
 
     def draw(self, surface):
         pygame.draw.rect(surface, (255, 0, 0), self.rect)
