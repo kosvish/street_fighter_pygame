87b26b2939e34a80b8e8d5693850b9d81d7985a8
kosvish <mr.kocta@bk.ru>
2023-02-04 01:10:46 +0300

    expanded our cycle and fixed bugs

--- fighter.py
+++ fighter.py
@@ -14,11 +14,14 @@ class Fighter():
 
     def load_images(self, sprite_sheet, animation_steps):
         # достаём изображение из листа анимации
-        for animation in animation_steps:
+        animation_list = []
+        for y, animation in enumerate(animation_steps):
             temp_img_list = []
             for x in range(animation):
-                temp_img = sprite_sheet.subsurface(x * self.size, 0, self.size, self.size)
+                temp_img = sprite_sheet.subsurface(x * self.size, y * self.size, self.size, self.size)
                 temp_img_list.append(temp_img)
+            animation_list.append(temp_img_list)
+        return animation_list
 
     def move(self, screen_width, screen_height, surface, target):
         SPEED = 10
