a92d3a59c86c0671ee6be90b9c9c3ee877baa0be
kosvish <mr.kocta@bk.ru>
2023-02-09 16:47:42 +0300

    creating a method to update animation frames

--- fighter.py
+++ fighter.py
@@ -2,14 +2,16 @@ import pygame
 
 
 class Fighter():
-    def __init__(self, x, y, data, sprite_sheet, animation_steps):
+    def __init__(self, x, y, flip, data, sprite_sheet, animation_steps):
         self.size = data[0]
         self.image_scale = data[1]
-        self.flip = False
+        self.offset = data[2]
+        self.flip = flip
         self.animation_list = self.load_images(sprite_sheet, animation_steps)
         self.action = 0  # 0: Стоит,  1:бежит, 2:прыгает, 3:атака1, 4:атака2, 5:получение удара, 6: смерть
         self.frame_index = 0
         self.image = self.animation_list[self.action][self.frame_index]
+        self.update_time = pygame.time.get_ticks()
         self.rect = pygame.Rect((x, y, 80, 180))
         self.vel_y = 0
         self.jump = False
@@ -84,6 +86,15 @@ class Fighter():
         self.rect.x += dx
         self.rect.y += dy
 
+    # обновление анимации
+    def update(self):
+        animation_cooldown = 500
+        self.image = self.animation_list[self.action][self.frame_index]
+        # проверка прошло ли время с момента последнего обновления кадра
+        if pygame.time.get_ticks() - self.update_time > animation_cooldown:
+            self.frame_index += 1
+            self.update_time = pygame.time.get_ticks()
+
     def attack(self, surface, target):
         self.attacking = True
         attacking_rect = pygame.Rect(
@@ -95,5 +106,8 @@ class Fighter():
         pygame.draw.rect(surface, (0, 255, 0), attacking_rect)
 
     def draw(self, surface):
+        img = pygame.transform.flip(self.image, self.flip, False)
         pygame.draw.rect(surface, (255, 0, 0), self.rect)
-        surface.blit(self.image, (self.rect.x, self.rect.y))
+        surface.blit(img, (
+            self.rect.x - (self.offset[0] * self.image_scale),
+            self.rect.y - (self.offset[1] * self.image_scale)))
